fede50b05d605153d68dcd9b1c77b09766afeb64
Ricardo Rocha <me@rrocha.uk>
2023-01-14 22:11:36 +0000

    * refactor into multiple files and better canvas management;

---
+++ client/src/constants.ts
@@ -0,0 +1,6 @@
+export enum constants {
+  BOARD_SIZE = 20,
+  GAP = 3,
+  CELL_SIZE = 15,
+  CANVAS_SIZE = BOARD_SIZE * CELL_SIZE + BOARD_SIZE * GAP - GAP,
+}
--- client/src/prepare-boards.ts
+++ client/src/prepare-boards.ts
@@ -1,48 +1,19 @@
-import { BoardType, ICell } from './types';
-import { request } from './request';
+import { ICell } from "./types";
+import { constants } from "./constants";
 
-const BOARD_SIZE = 20;
-const GAP = 3;
-const CELL_SIZE = 15;
-const CANVAS_SIZE = (BOARD_SIZE * CELL_SIZE) + (BOARD_SIZE * GAP) - GAP;
-
-export function prepareBoards(element: HTMLCanvasElement, board: BoardType) {
-  if ( !element ) throw new Error('no canvas passed in');
-
-  element.width = CANVAS_SIZE;
-  element.height = CANVAS_SIZE;
-
-  const c: CanvasRenderingContext2D | null = element.getContext('2d');
-
-  if ( !c ) throw new Error('no canvas context found');
-
+export function prepareBoards(ctx: CanvasRenderingContext2D) {
   const cells: ICell[] = [];
 
-  for ( let i = 0; i < BOARD_SIZE; i++ ) {
-    for ( let j = 0; j < BOARD_SIZE; j++ ) {
-      const x = (CELL_SIZE * j) + (j * GAP);
-      const y = (CELL_SIZE * i) + (i * GAP);
-      c.beginPath();
-      c.fillStyle = 'black';
-      c.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+  for (let i = 0; i < constants.BOARD_SIZE; i++) {
+    for (let j = 0; j < constants.BOARD_SIZE; j++) {
+      const x = constants.CELL_SIZE * j + j * constants.GAP;
+      const y = constants.CELL_SIZE * i + i * constants.GAP;
+      ctx.beginPath();
+      ctx.fillStyle = "black";
+      ctx.fillRect(x, y, constants.CELL_SIZE, constants.CELL_SIZE);
       cells.push({ x: x, y: y, sq: `${i} - ${j}` });
     }
   }
 
-  if ( board === 'opponent' ) {
-    element.addEventListener('click', (e: MouseEvent) => {
-      const cell: ICell | undefined = cells.find((cell: any) => {
-        return (e.offsetX > cell.x + GAP)
-          && (e.offsetX < cell.x + CELL_SIZE + GAP)
-          && (e.offsetY > cell.y + GAP)
-          && (e.offsetY < cell.y + CELL_SIZE + GAP);
-      });
-      if ( cell ) {
-        c.beginPath();
-        c.fillStyle = 'red';
-        c.fillRect(cell.x, cell.y, CELL_SIZE, CELL_SIZE);
-        request(cell);
-      }
-    });
-  }
+  return { cells };
 }
